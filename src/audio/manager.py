9386cff35277b0c491f38a519aee2bac3fda0560
Xenox003 <xenox003yt@gmail.com>
2022-08-20 20:23:29 +0200

    Finished connection managing

--- src/audio/manager.py
+++ src/audio/manager.py
@@ -12,6 +12,7 @@ class AudioManager():
         self.running = True
         
         self.connections = {}
+        self.ignores = []
         self.client = jack.Client("AudioManager")
         self.run()
         
@@ -35,13 +36,43 @@ class AudioManager():
         if sink in self.connections[source]:
             self.connections[source].remove(sink)
         
+    def update_connections(self):
+        ports = {}
+        connections = {}
+        
+        for port in self.client.get_ports(is_audio=True):
+            ports[port.name] = port
+            connections[port.name] = list(c.name for c in self.client.get_all_connections(port))
+            
+        for sink in self.get_sinks():
+            del connections[sink]
+            
+        for source, sinks in connections.items():
+            for sink in sinks:
+                if source not in self.connections or sink not in self.connections[source]:
+                    if any([ign('source',source) or ign('sink', sink) for ign in self.ignores]):
+                        continue
+                    self.client.disconnect(source,sink)
+                    
+        for source, sinks in self.connections.items():
+            for sink in sinks:
+                skip = False
+
+                if sink not in ports:
+                    skip=True
+
+                if source not in ports:
+                    skip=True
+
+                if not skip and sink not in connections[source]:
+                    print("Connecting '%s' and '%s'" % (source, sink))
+                    self.client.connect(source, sink)
+        
     def run(self):
         def target():
             while self.running:
                 try:
-                    #self.ensure_connections()
-                    print(self.get_sources())
-                    print(self.get_sinks())
+                    self.update_connections()
                 except:
                     print("Error handling connections")
                     traceback.print_exc()
